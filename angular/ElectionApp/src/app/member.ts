873f871c5002f464d26e06e6a771b2ba7901eb11
yumajen <50066616+yumajen@users.noreply.github.com>
2019-09-04 01:30:13 +0900

    中間報告ページ作成_スタイル調整 (#4)

--- angular/ElectionApp/src/app/member.ts
+++ angular/ElectionApp/src/app/member.ts
@@ -2,14 +2,15 @@ export class Member {
     id: number;
     name: string;
     prof: string;
+    voted: number;
 
     static members: Member[] = [
-        { id: 1, name: 'Samidare', prof: 'My name is "Samidare". Nice to meet you.' },
-        { id: 2, name: 'Maya', prof: 'My name is "Maya".' },
-        { id: 3, name: 'Kiso', prof: 'I\'ll give you the Great Victory.' },
-        { id: 4, name: 'Sendai', prof: 'My name is "Sendai".' },
-        { id: 5, name: 'Akagi', prof: 'My name is "Akagi".' },
-        { id: 6, name: 'Hyuga', prof: 'My name is "Hyuga".' },
-        { id: 7, name: 'Jyunyo', prof: 'My name is "Jyunto".' }
+        { id: 1, name: 'Samidare', prof: 'My name is "Samidare". Nice to meet you.', voted: 5 },
+        { id: 2, name: 'Maya', prof: 'My name is "Maya".', voted: 6 },
+        { id: 3, name: 'Kiso', prof: 'I\'ll give you the Great Victory.', voted: 1 },
+        { id: 4, name: 'Sendai', prof: 'My name is "Sendai".', voted: 2 },
+        { id: 5, name: 'Akagi', prof: 'My name is "Akagi".', voted: 1 },
+        { id: 6, name: 'Hyuga', prof: 'My name is "Hyuga".', voted: 3 },
+        { id: 7, name: 'Jyunyo', prof: 'My name is "Jyunto".', voted: 5 }
     ]
 }
--- angular/ElectionApp/src/app/poll/poll.component.ts
+++ angular/ElectionApp/src/app/poll/poll.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member';
 
 @Component({
   selector: 'app-poll',
@@ -12,4 +13,37 @@ export class PollComponent implements OnInit {
   ngOnInit() {
   }
 
+  findByVoted(voted: number): Member[] {
+    let foundMembers = [];
+
+    // 同じvotedの値のmemberを1つのグループにまとめる
+    for (let member of Member.members) {
+      if (member.voted == voted) {
+        foundMembers.push(member);
+      }
+    }
+
+    return foundMembers;
+  }
+
+  reportPoll(): Member[] {
+    // Member.members配列をシャローコピー後に降順にソートする
+    let sortedMembers = (Member.members.slice()).sort(
+      (member1, member2) => member2.voted - member1.voted
+    );
+    let maxVote = sortedMembers[0].voted;
+    let membersToReport: Member[] = [];
+
+    for (let voted = maxVote; voted >= 0; voted--) {
+      let groupMembers: Member[] = this.findByVoted(voted);
+      if (groupMembers.length > 0) {
+        membersToReport = membersToReport.concat(groupMembers);
+      }
+      if (membersToReport.length >= 4) {
+        break;
+      }
+    }
+
+    return membersToReport;
+  }
 }
